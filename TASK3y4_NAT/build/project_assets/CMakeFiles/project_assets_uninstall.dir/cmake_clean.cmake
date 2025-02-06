2b1f18fda6e10cc27cfb0f9c2093660191fa1731
nataliasemperess <al415537@uji.es>
2025-02-06 19:07:00 +0100

    cambios

---
+++ TASK3y4_NAT/build/project_assets/CMakeFiles/project_assets_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_assets_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/project_assets_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ TASK3y4_NAT/rmf_ws/src/project/build/project_config/colcon_command_prefix_build.sh
@@ -0,0 +1,4 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/usuario/Documentos/GitHub/IR2134/TASK3y4_NAT/rmf_ws/src/project/install/project_assets/share/project_assets/package.sh"
+. "/home/usuario/Documentos/GitHub/IR2134/TASK3y4_NAT/rmf_ws/src/project/install/project_fleet_adapter/share/project_fleet_adapter/package.sh"
+. "/home/usuario/Documentos/GitHub/IR2134/TASK3y4_NAT/rmf_ws/src/project/install/project_maps/share/project_maps/package.sh"
