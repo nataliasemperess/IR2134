38fbb9e9163f45856793bba578a8b24608086bfa
nataliasemperess <113502742+nataliasemperess@users.noreply.github.com>
2025-02-11 18:50:59 +0100

    cambios

--- TASK3y4_NAT/rmf_ws/src/project/build/project_config/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/usuario/Documentos/GitHub/IR2134/TASK3y4_NAT/rmf_ws/src/project/project_config
-# Build directory: /home/usuario/Documentos/GitHub/IR2134/TASK3y4_NAT/rmf_ws/src/project/build/project_config
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- TASK3y4_NAT/rmf_ws/src/project/build/project_maps/CMakeFiles/project_maps_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/project_maps_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/project_maps_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
