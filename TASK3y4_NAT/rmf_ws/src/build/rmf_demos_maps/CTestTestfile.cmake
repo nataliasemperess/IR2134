20197c05069638458c6c345b43aacd1348188240
nataliasemperess <113502742+nataliasemperess@users.noreply.github.com>
2025-02-11 19:28:39 +0100

    cambios

---
+++ TASK3y4_NAT/rmf_ws/src/build/rmf_demos_maps/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/usuario/Documentos/GitHub/IR2134/TASK3y4_NAT/src/rmf_demos_maps
+# Build directory: /home/usuario/Documentos/GitHub/IR2134/TASK3y4_NAT/src/build/rmf_demos_maps
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
