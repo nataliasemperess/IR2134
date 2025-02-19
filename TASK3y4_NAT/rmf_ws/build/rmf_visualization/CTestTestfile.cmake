a9521d498dfb45f81f7a32dba07cd094dda50248
nataliasemperess <al415537@uji.es>
2025-02-19 14:01:06 +0100

    CAMBIOS BUENOS HASTA DASHBOARD

--- TASK3y4_NAT/rmf_ws/build/rmf_visualization/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/usuario/rmf_ws/src/rmf_visualization
-# Build directory: /home/usuario/rmf_ws/build/rmf_visualization
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
