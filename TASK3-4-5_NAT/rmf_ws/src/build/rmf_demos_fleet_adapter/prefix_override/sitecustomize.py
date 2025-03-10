bb380c52fb2455ea7532c58ff06f46ba16bf1e53
nataliasemperess <al415537@uji.es>
2025-03-10 19:59:09 +0100

    problemas con rviz

--- TASK3-4-5_NAT/rmf_ws/src/build/rmf_demos_fleet_adapter/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/usuario/Documentos/GitHub/IR2134/TASK3y4_NAT/src/install/rmf_demos_fleet_adapter'
--- TASK3-4-5_NAT/rmf_ws/src/build/rmf_demos_maps/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/usuario/Documentos/GitHub/IR2134/TASK3y4_NAT/src/rmf_demos_maps
-# Build directory: /home/usuario/Documentos/GitHub/IR2134/TASK3y4_NAT/src/build/rmf_demos_maps
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
